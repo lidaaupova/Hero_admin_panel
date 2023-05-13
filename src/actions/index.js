49741728d704efc1c9e5aef326d01ff2ecd4341f
Lida Aupova <lida.aupova@gmail.com>
2023-05-13 20:16:15 +0300

    Оптимизировано создание reducers и actionCreaters через Redux Toolkit и функцию createSlice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+import { heroesFetching, heroesFetched, heroesFetchingError } from "../components/heroesList/heroesSlice";
+import { filtersFetching, filtersFetched, filtersFetchingError } from "../components/heroesFilters/filtersSlice";
+
 //Комплексный actionCreator для получения героев с сервера и обработки разных состояний
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
@@ -6,77 +9,9 @@ export const fetchHeroes = (request) => (dispatch) => {
         .catch(() => dispatch(heroesFetchingError()))
 }
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
-
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
-
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
-
 export const fetchFilters = (request) => (dispatch) => {
-    dispatch(filterFetching());
+    dispatch(filtersFetching());
         request('http://localhost:3001/filters')
-            .then(data => dispatch(filterFetched(data)))
-            .catch(() => dispatch(filterFetchingError()))
-}
-
-export const filterFetching = () => {
-    return {
-        type: 'FILTER_FETCHING'
-    }
-}
-
-export const filterFetched = (filters) => {
-    return {
-        type: 'FILTER_FETCHED',
-        payload: filters
-    }
-}
-
-export const filterFetchingError = () => {
-    return {
-        type: 'FILTER_FETCHING_ERROR'
-    }
-}
-
-export const activeFilterChanged = (filter) => {
-    return {
-        type: 'ACTIVE_FILTER_CHANGED',
-        payload: filter
-    }
-}
-
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//     setTimeout(() => {
-//         dispatch({
-//             type: 'ACTIVE_FILTER_CHANGED',
-//             payload: filter
-//         })
-//     }, 1000)
-// }
-
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
-    }
-}
-
-export const heroCreated = (data) => {
-    return {
-        type: "HERO_CREATED",
-        payload: data
-    }
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()))
 }
--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -13,7 +13,7 @@ import { useHttp } from "../../hooks/http.hook";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 
-import { heroCreated } from "../../actions";
+import { heroCreated } from "../heroesList/heroesSlice";
 
 const HeroesAddForm = () => {
     // Состояния для контроля формы
